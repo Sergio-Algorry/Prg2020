59298211bc15a540d10bab59520ba059240578e9
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-05-13 10:32:22 -0300

    Primer formulario

---
+++ HolaMundo/HolaAula.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace HolaMundo
+{
+    partial class HolaAula
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.lblEtiqueta = new System.Windows.Forms.Label();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Location = new System.Drawing.Point(207, 26);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btAceptar.TabIndex = 0;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Location = new System.Drawing.Point(12, 26);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(189, 20);
+            this.txtMateria.TabIndex = 1;
+            // 
+            // lblEtiqueta
+            // 
+            this.lblEtiqueta.AutoSize = true;
+            this.lblEtiqueta.Location = new System.Drawing.Point(9, 10);
+            this.lblEtiqueta.Name = "lblEtiqueta";
+            this.lblEtiqueta.Size = new System.Drawing.Size(42, 13);
+            this.lblEtiqueta.TabIndex = 2;
+            this.lblEtiqueta.Text = "Materia";
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(9, 59);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(29, 13);
+            this.lblMensaje.TabIndex = 3;
+            this.lblMensaje.Text = "Hola";
+            // 
+            // HolaAula
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 140);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.lblEtiqueta);
+            this.Controls.Add(this.txtMateria);
+            this.Controls.Add(this.btAceptar);
+            this.Name = "HolaAula";
+            this.Text = "Hola Gente del Aula";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.Label lblEtiqueta;
+        private System.Windows.Forms.Label lblMensaje;
+    }
+}
---
+++ HolaMundo/HolaAula.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundo
+{
+    public partial class HolaAula : Form
+    {
+        public HolaAula()
+        {
+            InitializeComponent();
+        }
+
+        private void btAceptar_Click(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "Hola gente de la materia " + txtMateria.Text;
+        }
+    }
+}
