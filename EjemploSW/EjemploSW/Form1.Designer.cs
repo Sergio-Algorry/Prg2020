0976d046928e5a2290f3c10618c8bce62c6a3a92
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-06-02 12:12:56 -0300

    Switch

---
+++ EjemploSW/EjemploSW/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace EjemploSW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.lblE1 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.txtNota = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Location = new System.Drawing.Point(182, 66);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btAceptar.TabIndex = 0;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
+            // lblE1
+            // 
+            this.lblE1.AutoSize = true;
+            this.lblE1.Location = new System.Drawing.Point(73, 49);
+            this.lblE1.Name = "lblE1";
+            this.lblE1.Size = new System.Drawing.Size(30, 13);
+            this.lblE1.TabIndex = 1;
+            this.lblE1.Text = "Nota";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.Location = new System.Drawing.Point(73, 102);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 24);
+            this.lblRes.TabIndex = 2;
+            // 
+            // txtNota
+            // 
+            this.txtNota.Location = new System.Drawing.Point(76, 66);
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(100, 20);
+            this.txtNota.TabIndex = 3;
+            this.txtNota.TextChanged += new System.EventHandler(this.txtEdad_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 190);
+            this.Controls.Add(this.txtNota);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.lblE1);
+            this.Controls.Add(this.btAceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.Label lblE1;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.TextBox txtNota;
+    }
+}
+
---
+++ EjemploSW/EjemploSW/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploSW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAceptar_Click(object sender, EventArgs e)
+        {
+                int nota = System.Convert.ToInt32(txtNota.Text);
+                switch (nota)
+                {
+                    case 1:
+                        lblRes.Text = "Aplazado";
+                        break;
+                    case 2:
+                        lblRes.Text = "Aplazado";
+                        break;
+                    case 3:
+                        lblRes.Text = "Aprobado";
+                        break;
+                    case 4:
+                        lblRes.Text = "Promocionado";
+                        break;
+                    case 5:
+                        lblRes.Text = "Promocionado";
+                        break;
+                    default:
+                        lblRes.Text = "Nota no válida";
+                        break;
+                }
+
+
+            //switch (txtNota.Text.Trim())
+            //{
+            //    case "1":
+            //        lblRes.Text = "Aplazado";
+            //        break;
+            //    case "2":
+            //        lblRes.Text = "Aplazado";
+            //        break;
+            //    case "3":
+            //        lblRes.Text = "Aprobado";
+            //        break;
+            //    case "4":
+            //        lblRes.Text = "Promocionado";
+            //        break;
+            //    case "5":
+            //        lblRes.Text = "Promocionado";
+            //        break;
+            //    default:
+            //        lblRes.Text = "Nota no válida";
+            //        break;
+            //}
+
+        }
+
+        private void txtEdad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
