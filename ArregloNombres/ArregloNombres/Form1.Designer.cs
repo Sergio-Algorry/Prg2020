26ae55d75bf4694f5b133ef819400554905b88a7
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-06-23 11:54:39 -0300

    Arreglos

---
+++ ArregloNombres/ArregloNombres/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ArregloNombres
+{
+    partial class frmNombres
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btIngresar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblLista = new System.Windows.Forms.Label();
+            this.btListar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btDimensionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btIngresar
+            // 
+            this.btIngresar.Location = new System.Drawing.Point(313, 35);
+            this.btIngresar.Name = "btIngresar";
+            this.btIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btIngresar.TabIndex = 0;
+            this.btIngresar.Text = "Ingresar";
+            this.btIngresar.UseVisualStyleBackColor = true;
+            this.btIngresar.Click += new System.EventHandler(this.btIngresar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(68, 38);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(239, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblLista
+            // 
+            this.lblLista.AutoSize = true;
+            this.lblLista.Location = new System.Drawing.Point(13, 65);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(0, 13);
+            this.lblLista.TabIndex = 2;
+            // 
+            // btListar
+            // 
+            this.btListar.Location = new System.Drawing.Point(313, 65);
+            this.btListar.Name = "btListar";
+            this.btListar.Size = new System.Drawing.Size(75, 23);
+            this.btListar.TabIndex = 3;
+            this.btListar.Text = "Listar";
+            this.btListar.UseVisualStyleBackColor = true;
+            this.btListar.Click += new System.EventHandler(this.btListar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(68, 12);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(93, 20);
+            this.txtCantidad.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cantidad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "nombre";
+            // 
+            // btDimensionar
+            // 
+            this.btDimensionar.Location = new System.Drawing.Point(313, 8);
+            this.btDimensionar.Name = "btDimensionar";
+            this.btDimensionar.Size = new System.Drawing.Size(75, 23);
+            this.btDimensionar.TabIndex = 7;
+            this.btDimensionar.Text = "Dimension";
+            this.btDimensionar.UseVisualStyleBackColor = true;
+            this.btDimensionar.Click += new System.EventHandler(this.btDimensionar_Click);
+            // 
+            // frmNombres
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 450);
+            this.Controls.Add(this.btDimensionar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.btListar);
+            this.Controls.Add(this.lblLista);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btIngresar);
+            this.Name = "frmNombres";
+            this.Text = "Nombres";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btIngresar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblLista;
+        private System.Windows.Forms.Button btListar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btDimensionar;
+    }
+}
+
---
+++ ArregloNombres/ArregloNombres/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArregloNombres
+{
+    public partial class frmNombres : Form
+    {
+        string[] Lista;
+        int Posicion = 0;
+
+        public frmNombres()
+        {
+            InitializeComponent();
+            btIngresar.Enabled = false;
+            btListar.Enabled = false;
+        }
+
+        private void btIngresar_Click(object sender, EventArgs e)
+        {
+            Lista[Posicion] = txtNombre.Text;
+            Posicion = Posicion + 1;
+            
+            //lblLista.Text = lblLista.Text + txtNombre.Text + "\r\n";
+
+            txtNombre.Text = "";
+            txtNombre.Focus();
+        }
+
+        private void btListar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Lista.Length; i++)
+            {
+                lblLista.Text = lblLista.Text + Lista[i] + "\r\n";
+            }
+        }
+
+        private void btDimensionar_Click(object sender, EventArgs e)
+        {
+            Lista = new string[System.Convert.ToInt32(txtCantidad.Text) ];
+            Posicion = 0;
+            btIngresar.Enabled = true;
+            btListar.Enabled = true;
+        }
+    }
+}
