2701f71d5494eb7a679c87e18e3b9c65ca9710b6
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-08-19 09:51:23 -0300

    CargaDatos2

---
+++ CargaDatosV2/CargaDatos/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CargaDatos
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(12, 14);
+            this.txtDato.Multiline = true;
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(205, 20);
+            this.txtDato.TabIndex = 0;
+            // 
+            // btCargar
+            // 
+            this.btCargar.Location = new System.Drawing.Point(223, 14);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(75, 23);
+            this.btCargar.TabIndex = 1;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.btCargar_Click);
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Location = new System.Drawing.Point(30, 53);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(35, 13);
+            this.lblSalida.TabIndex = 2;
+            this.lblSalida.Text = "Datos";
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 450);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.txtDato);
+            this.Name = "frmInicio";
+            this.Text = "Cargador de Datos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btCargar;
+        private System.Windows.Forms.Label lblSalida;
+    }
+}
+
