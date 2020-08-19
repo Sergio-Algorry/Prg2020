07f3ad1e4c2a081e86722418f75086844b786b46
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-08-19 09:16:38 -0300

    CargaDatos 1

---
+++ CargaDatos/CargaDatos/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CargaDatos
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.btIniciar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(12, 78);
+            this.txtDato.Multiline = true;
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(205, 20);
+            this.txtDato.TabIndex = 0;
+            // 
+            // btCargar
+            // 
+            this.btCargar.Enabled = false;
+            this.btCargar.Location = new System.Drawing.Point(223, 78);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(75, 23);
+            this.btCargar.TabIndex = 1;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.btCargar_Click);
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Location = new System.Drawing.Point(30, 117);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(35, 13);
+            this.lblSalida.TabIndex = 2;
+            this.lblSalida.Text = "Datos";
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.Location = new System.Drawing.Point(223, 29);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btIniciar.TabIndex = 3;
+            this.btIniciar.Text = "Iniciar";
+            this.btIniciar.UseVisualStyleBackColor = true;
+            this.btIniciar.Click += new System.EventHandler(this.btIniciar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(12, 31);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cantidad de datos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Dato";
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.btIniciar);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.txtDato);
+            this.Name = "frmInicio";
+            this.Text = "Cargador de Datos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btCargar;
+        private System.Windows.Forms.Label lblSalida;
+        private System.Windows.Forms.Button btIniciar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CargaDatos/CargaDatos/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargaDatos
+{
+    public partial class frmInicio : Form
+    {
+        string[] Datos;
+        int Indice = 0;
+
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+            if (Indice < Datos.Length)
+            {
+                Datos[Indice] = txtDato.Text;
+                Indice = Indice + 1;
+            }
+            else
+            {
+                lblSalida.Text = "El dato " + txtDato.Text + " no se cargó por exceder el límite";
+            }
+            txtDato.Focus();
+            txtDato.SelectAll();
+            //txtDato.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btIniciar_Click(object sender, EventArgs e)
+        {
+            Datos = new string[System.Convert.ToInt32( txtCantidad.Text)];
+            Indice = 0;
+
+            btCargar.Enabled = true;
+
+            txtDato.Focus();
+        }
+    } 
+}
