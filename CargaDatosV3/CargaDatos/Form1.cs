aae2be46d36fe02929ffc66cb1e74be0a213f42f
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-08-25 09:55:46 -0300

    CargaDatosV3 inicio
    Se plantea presentar los datos cargados e incorporar la carga de otra lista

---
+++ CargaDatosV3/CargaDatos/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargaDatos
+{
+    public partial class frmInicio : Form
+    {
+        string[] Datos = new string[1];
+        int Indice = 0;
+
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+            if (Indice < Datos.Length)
+            {
+                Datos[Indice] = txtDato.Text;
+                Indice = Indice + 1;
+            }
+            else
+            {
+                string[] temp = new string[Datos.Length + 1];
+
+                for (int i = 0; i < Datos.Length; i++)
+                {
+                    temp[i] = Datos[i];
+                }
+
+                Datos = temp;
+
+                Datos[Indice] = txtDato.Text;
+                Indice = Indice + 1;
+            }
+            txtDato.Focus();
+            txtDato.SelectAll();
+
+            //string pepe = "un dato";
+            //string juan = "otro dato";
+
+            //int i = pepe.CompareTo(juan);
+
+        }
+
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void btIniciar_Click(object sender, EventArgs e)
+        //{
+        //    Datos = new string[System.Convert.ToInt32( txtCantidad.Text)];
+        //    Indice = 0;
+
+        //    btCargar.Enabled = true;
+
+        //    txtDato.Focus();
+        //}
+    } 
+}
