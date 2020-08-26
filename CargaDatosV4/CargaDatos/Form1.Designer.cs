032e87c3ceeb72ec225786f1cda5bff941693c6b
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-08-26 08:37:56 -0300

    Version 4 inicio - POO

---
+++ CargaDatosV4/CargaDatos/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CargaDatos
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 28);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(205, 21);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // btCargar
+            // 
+            this.btCargar.Location = new System.Drawing.Point(223, 72);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(75, 23);
+            this.btCargar.TabIndex = 2;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.btCargar_Click);
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Location = new System.Drawing.Point(17, 101);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(35, 13);
+            this.lblSalida.TabIndex = 3;
+            this.lblSalida.Text = "Datos";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(12, 72);
+            this.txtApellido.Multiline = true;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(205, 20);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Apellido";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(22, 426);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 6;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "frmInicio";
+            this.Text = "Cargador de Datos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btCargar;
+        private System.Windows.Forms.Label lblSalida;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ CargaDatosV4/CargaDatos/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargaDatos
+{
+    public partial class frmInicio : Form
+    {
+        string[] Nombres = new string[1];
+        string[] Apellidos = new string[1];
+
+        //Lista ListaDeNombres = new Lista();
+        //Lista ListaDeApellidos = new Lista();
+
+        int Indice = 0;
+
+        public frmInicio()
+        {
+            InitializeComponent();
+            //ListaDeNombres.Redimension();
+            //ListaDeApellidos.Redimension();
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+            if (Indice < Nombres.Length)
+            {
+                CargaDatos();
+            }
+            else
+            {
+                //actualizo la dimensión de Nombres
+                Nombres = Redimension(Nombres);
+
+                //actualizo la dimensión de Apellidos
+                //string[] tempa = new string[Apellidos.Length + 1];
+                //for (int i = 0; i < Apellidos.Length; i++)
+                //{
+                //    tempa[i] = Apellidos[i];
+                //}
+                //Apellidos = tempa;
+                Apellidos = Redimension(Apellidos);
+
+                CargaDatos();
+            }
+            txtNombre.Focus();
+            txtNombre.SelectAll();
+        }
+
+        private string[] Redimension(string[] Dato)
+        {
+            string[] temp = new string[Dato.Length + 1];
+            for (int i = 0; i < Dato.Length; i++)
+            {
+                temp[i] = Dato[i];
+            }
+            Dato = temp;
+
+            return Dato;
+        }
+
+        private void CargaDatos()
+        {
+            Nombres[Indice] = txtNombre.Text;
+            Apellidos[Indice] = txtApellido.Text;
+            Indice = Indice + 1;
+            lblSalida.Text = lblSalida.Text + "\r\n" + txtNombre.Text + " " + txtApellido.Text;
+        }
+    }
+
+    //public class Lista
+    //{
+    //    public string[] Redimension(string[] Dato)
+    //    {
+    //        string[] temp = new string[Dato.Length + 1];
+    //        for (int i = 0; i < Dato.Length; i++)
+    //        {
+    //            temp[i] = Dato[i];
+    //        }
+    //        Dato = temp;
+
+    //        return Dato;
+    //    }
+
+    //    public string[] OrdenarAlfabeticamente()
+    //    {
+    //        return new string[1];
+    //    }
+
+    //    public string Listar()
+    //    {
+    //    }
+
+    //    public string[] agregar()
+    //    {
+
+    //    }
+    //}
+}
